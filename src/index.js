97a74e531f2e7403f2056d8383d553be8d8e5e06
LGmedia954 <LeslieGonzalez@me.com>
2021-11-12 16:58:13 -0500

    test

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const endPoint = "http://localhost:3000/api/v1/vocabulary_words"
+
+document.addEventListener('DOMContentLoaded', () => {
+  getVocab()
+  // Sentence.listenForEvents()
+  // Sentence.formHandler(e)
+  const createSentenceForm = document.querySelector("#create-sentence-form")
+  createSentenceForm.addEventListener("submit", (e) => createFormHandler(e))
+})
+
+function getVocab() {
+  fetch(endPoint)
+  .then(response => response.json())
+  .then(vocab => {
+    vocab.data.forEach(vocabulary_word => {
+      const vMarkup = `
+        <div data-id=${vocabulary_word.id}>
+          <h2>${vocabulary_word.attributes.word}</h2>
+          <i>${vocabulary_word.attributes.part_of_speech}</i>
+          <p>${vocabulary_word.attributes.definition}</p>
+          <button onclick="showForm()">Try It</button>
+
+          <div class="form-container"><br>
+            <form id="create-sentence-form" style="">
+            <input id='input-example' type="text" size="100" name="example" value="" placeholder="Enter your sentence here." class="input-text">
+            <input id='input-vocabulary_word_id' type="hidden" name="vocabulary_word_id" value="">
+            <br><br>
+            <input id= 'create-button' type="submit" name="submit" value="Create New Sentence" class="submit">
+            </form>
+          </div>
+        <br><br>`;
+
+        document.querySelector('#v-container').innerHTML += vMarkup
+      })
+    })
+ }
+
+  function createFormHandler(e) {
+    e.preventDefault()
+    debugger
+    // console.log(e);
+  }
---
+++ src/sentence.js
@@ -0,0 +1,32 @@
+// const newSentence = document.querySelector("#new-sentence")
+// const createSentenceForm = document.querySelector("#create-sentence-form")
+
+class Sentence {
+
+  // static listenForEvents() {
+  //   newSentence.addEventListener('click', this.showForm)
+  //   sentenceForm.addEventListener('submit', (e) => Sentence.createSentence(e))
+  // }
+
+  // static showForm() {
+  //   newSentence.style.display="none"
+  //   sentenceForm.style.display="block"
+  // }
+
+// function showForm() {
+//   let x = document.getElementById("sentenceForm");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
+
+  // static createSentence(e) {
+  //   e.preventDefault()
+  //   let example = document.querySelector("#example").value
+  //   let vwId = document.querySelector("#vocabulary_word_id").value
+  // }
+
+}
+
