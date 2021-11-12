acc17bd2a8b484532e4804139c7a86c9bc180213
LGmedia954 <LeslieGonzalez@me.com>
2021-11-11 20:51:56 -0500

    initial fetch method before refactor

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("in index.js");
+const endPoint = "http://localhost:3000/api/v1/vocabulary_words"
+
+document.addEventListener('DOMContentLoaded', () => {
+  getVocab()
+})
+
+function getVocab() {
+  fetch(endPoint)
+  .then(response => response.json())
+  .then(vocab => {
+    vocab.data.forEach(vocabulary_word => {
+      
+      const vMarkup = `
+        <div data-id=${vocabulary_word.id}>
+          <h2>${vocabulary_word.attributes.word}</h2>
+          <i>${vocabulary_word.attributes.part_of_speech}</i>
+          <p>${vocabulary_word.attributes.definition}</p>
+          <button data-id=${vocabulary_word.id}>Try It</button>
+        </div>
+        <br><br>`;
+
+        document.querySelector('#v-container').innerHTML += vMarkup
+      })
+    })
+ }
+
